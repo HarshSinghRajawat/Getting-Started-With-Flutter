ac726a191c078a3935c89bf0903792332d4bc039
HarshSinghRajawat <harshsinghrajawat86@gmail.com>
2021-03-13 09:53:49 +0530

    Basics

--- lib/main.dart
+++ lib/main.dart
@@ -4,29 +4,104 @@ void main()=>runApp(MaterialApp(
   home: Home(),
 ));
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  int level=0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[900],
       appBar: AppBar(
+        title: Text('ID Project'),
         centerTitle: true,
-        backgroundColor: Colors.orangeAccent[400],
-        title: Text('Hello World'),
-      ),
-      body: Center(
-        child: Text('Hello Programmer!',
-          style: TextStyle(
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2.0,
-            color: Colors.grey[600],
-          ),
-        ),
+        backgroundColor: Colors.grey[850],
+        elevation: 0.0,
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){},
-        child: Text('Click'),
-        backgroundColor: Colors.orangeAccent[400],
+        onPressed: (){
+          setState(() {
+            level++;
+          });
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Colors.grey[800],
+      ),
+      body:
+      Padding(
+        padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/thumb.jpg'),
+                radius: 40.0,
+              ),
+            ),
+            Divider(
+              height: 90,
+              color: Colors.grey[800],
+            ),
+            Text(
+              'NAME',
+              style: TextStyle(
+                letterSpacing: 2.0,
+                color: Colors.grey,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Harsh Singh Rajawat',
+              style: TextStyle(
+                fontSize: 29,
+                letterSpacing: 2.0,
+                color: Colors.amber,
+                fontWeight: FontWeight.bold,
+              ),
+            ),SizedBox(height: 20,),
+            Text(
+              'Current Level',
+              style: TextStyle(
+                letterSpacing: 2.0,
+                color: Colors.grey,
+              ),
+            ),
+            SizedBox(height: 10,),
+            Text(
+              '$level',
+              style: TextStyle(
+                fontSize: 29,
+                letterSpacing: 2.0,
+                color: Colors.amber,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 30,),
+            Row(
+              children: [
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[400],
+                ),
+                SizedBox(width: 10,),
+                Text(
+                  'test@gmail.com',
+                  style: TextStyle(
+                    color: Colors.grey[400],
+                    fontSize: 19,
+                    letterSpacing: 1,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
